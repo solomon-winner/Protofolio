18c920fefe6096580118d9094c4fb513a943b0f3
solomon-winner <solyalew3@gmail.com>
2024-12-14 08:58:52 +0300

    ...editing my protofolio...

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ export const Banner = () => {
     const [text, setText] = useState('');
     const [delta,setDelta] = useState(300 - Math.random() * 100);
     const [index,setIndex] = useState(1);
-    const toRotate = [ "Web Developer", "Web Designer", "JavaScript Expertise" ];
+    const toRotate = [ "Web Developer", "Web Designer", "MERN stack Expertise" ];
     const period = 2000;
 
     useEffect(() => {
